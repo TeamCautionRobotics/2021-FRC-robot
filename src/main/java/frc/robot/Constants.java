4b20769987a736de63cc05a142cf61c14916de2c
cadenmr <epintric@gmail.com>
2021-02-06 10:54:25 -0700

    initial commit for Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,20 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static final int LEFT_JOYSTICK_PORT = 0;
+    public static final int RIGHT_JOYSTICK_PORT = 1;
+
+    public static final int LEFT_DRIVE_MOTOR_0_ID = 0;
+    public static final int LEFT_DRIVE_MOTOR_1_ID = 1;
+
+    public static final int RIGHT_DRIVE_MOTOR_0_ID = 2;
+    public static final int RIGHT_DRIVE_MOTOR_1_ID = 3;
+
+    public static final int CENTER_DRIVE_MOTOR_0_ID = 4;
+    public static final int CENTER_DRIVE_MOTOR_1_ID = 5;
+
+    
+
+}
