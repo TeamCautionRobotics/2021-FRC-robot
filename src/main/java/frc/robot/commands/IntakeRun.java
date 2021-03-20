f954c6d1f23fa1f1a2adc3f1f404a597cbcc25e8
cadenmr <epintric@gmail.com>
2021-03-20 10:33:57 -0700

    cleanup

--- src/main/java/frc/robot/commands/IntakeRun.java
+++ src/main/java/frc/robot/commands/IntakeRun.java
@@ -1,44 +1,36 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import frc.robot.subsystems.Intake;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-/** An example command that uses an example subsystem. */
 public class IntakeRun extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final Intake intakeSubsystem;
 
   /**
-   * Creates a new ExampleCommand.
+   * Creates a new IntakeRun.
    *
-   * @param subsystem The subsystem used by this command.
+   * @param intakeSubsystem The subsystem used by this command.
    */
   public IntakeRun(Intake intakeSubsystem) {
     this.intakeSubsystem = intakeSubsystem;
+
     addRequirements(intakeSubsystem);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     intakeSubsystem.setIntakeWheelPower(0.1);
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     intakeSubsystem.setIntakeWheelPower(0);
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
