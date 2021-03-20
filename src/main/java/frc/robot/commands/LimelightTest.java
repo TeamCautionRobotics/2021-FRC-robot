78d9e43b1e8fb698587ce230aa85b29c30e8a93c
cadenmr <epintric@gmail.com>
2021-03-20 16:02:35 -0700

    testing stuff

---
+++ src/main/java/frc/robot/commands/LimelightTest.java
@@ -0,0 +1,43 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Limelight;
+
+
+public class LimelightTest extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Limelight limelight;
+
+  Timer testTimer;
+
+  public LimelightTest(Limelight limelight) {
+
+    this.limelight = limelight;
+    testTimer = new Timer();
+    
+    addRequirements(limelight);
+  }
+
+  @Override
+  public void initialize() {
+    limelight.setLedMode(3);
+
+  }
+
+  @Override
+  public void execute() {
+    SmartDashboard.putNumber("limelight tx", limelight.getTx());
+    SmartDashboard.putNumber("limelight tv", limelight.getTv());
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
