9bed51eb2584b76ec0557e7b8b678c49039282e3
cadenmr <epintric@gmail.com>
2021-03-30 14:13:45 -0700

    add indexer subsystem

---
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Indexer extends SubsystemBase {
+
+  SpeedControllerGroup indexerMotor;
+
+  public Indexer(SpeedControllerGroup indexerMotor) {
+
+    this.indexerMotor = indexerMotor;
+
+  }
+
+  public void setIndexerMotor(double power) {
+    indexerMotor.set(power);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
