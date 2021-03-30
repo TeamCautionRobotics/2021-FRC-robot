4d002439926a2bf0a452024ddac643f763df09b3
cadenmr <epintric@gmail.com>
2021-03-30 10:52:35 -0700

    add missing commands

---
+++ src/main/java/frc/robot/commands/RunFlywheel.java
@@ -0,0 +1,43 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class RunFlywheel extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Shooter shooter;
+
+  /**
+   * Creates a new RunFlywheel.
+   *
+   * @param shooter The subsystem used by this command.
+   */
+  public RunFlywheel(Shooter shooter) {
+    this.shooter = shooter;
+
+    addRequirements(shooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    shooter.setFlywheelPower(0.7);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    shooter.setFlywheelPower(0.0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
